7d56b4366fb25e876dd4b2d356ba70028db8fc8f
Dogukan3648 <61602556+Dogukan3648@users.noreply.github.com>
2023-05-23 10:38:11 +0300

    Add files via upload

---
+++ Odev4.5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Doğukan");
+            myDictionary.Add(2, "Berivan");
+            myDictionary.Add(3, "Gülüzar");
+            myDictionary.Add(4, "Esra");
+            myDictionary.Add(5, "Armağan");
+
+            myDictionary.T1s();
+
+            myDictionary.T2s();
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
